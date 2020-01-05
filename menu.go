a44bce672929eb7952ab9280900372545a6459d9
Aron <aron.0504@hotmail.com>
2020-01-05 10:18:27 -0300

    menu finish add movie download cover

--- menu.go
+++ menu.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strconv"
 )
 
 func clearScreen() {
@@ -13,12 +14,16 @@ func clearScreen() {
 	cmd.Run()
 }
 
-func input() string {
+func input(message string) string {
+	fmt.Printf("%s\n\n", message)
+
 	input := bufio.NewReader(os.Stdin)
 	value, err := input.ReadString('\n')
 	if err != nil {
 		panic(err)
 	}
+
+	fmt.Println()
 
 	return clearString(value)
 }
@@ -26,29 +31,24 @@ func input() string {
 func menu() {
 	clearScreen()
 	fmt.Println("Movie Recommender")
-	fmt.Println()
-	fmt.Println("[1] Add a new movie")
+	fmt.Println("\n[1] Add a new movie")
 	fmt.Println("[2] Remove a movie")
-	fmt.Println()
-	fmt.Println("Select one of the options:")
-	fmt.Println()
 
-	value := input()
+	value := input("\nSelect one of the options")
 	switch value {
 	case "1":
 		menuAddNewMovie()
 	case "2":
 		fmt.Println("2")
+	default:
+		menu()
 	}
 }
 
 func menuAddNewMovie() {
 	clearScreen()
-	fmt.Println("What is the name of the movie?")
-	fmt.Println()
-	name := input()
-	fmt.Println()
 
+	name := input("What is the name of the movie?")
 	movies := getSearchMovies(name)
 	for i, movie := range movies {
 		fmt.Printf("[%d] %s (%d)\n", i+1, movie.Title, movie.Year)
@@ -58,15 +58,16 @@ func menuAddNewMovie() {
 		}
 	}
 
-	fmt.Println()
-	fmt.Println("Choose movie from list above or enter '0' to perform a new search.")
-	fmt.Println()
-
-	value := stringToInt(input())
+	value := stringToInt(input("\nChoose movie from list above or enter '0' to perform a new search."))
 	if value <= 0 || value > 20 || value > int64(len(movies)) {
 		menuAddNewMovie()
 	} else {
+		date := input("When did you watch this movie? Enter the date as follows YYYYMMDD")
+		if _, err := strconv.Atoi(date); err != nil || len(date) != 8 {
+			date = "00000000"
+		}
+
 		movie := getMovie(movies[value-1].IMDB)
-		fmt.Println(movie)
+		downloadCover(movie, date)
 	}
 }
