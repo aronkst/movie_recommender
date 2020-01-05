0fde90fd909eb697a4248cf4c85007d6a2fdca0e
Aron <aron.0504@hotmail.com>
2020-01-05 10:56:34 -0300

    read all movies

---
+++ database.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
+
+func readMovies() []string {
+	var database []string
+
+	folders, err := ioutil.ReadDir("./")
+	if err != nil {
+		panic(err)
+	}
+
+	for _, folder := range folders {
+		if folder.IsDir() && folder.Name() != ".git" {
+			files, err := ioutil.ReadDir(fmt.Sprintf("./%s/", folder.Name()))
+			if err != nil {
+				panic(err)
+			}
+			for _, file := range files {
+				imdb := strings.Split(file.Name(), "__")[1]
+				database = append(database, imdb)
+			}
+		}
+	}
+
+	return database
+}
