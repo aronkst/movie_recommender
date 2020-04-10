3aa0b4ab8d95ed967304903d588e2075d4a2dff6
Aron Stall <aron.0504@hotmail.com>
2020-04-10 09:51:42 -0300

    fix menu download cover

--- menu.go
+++ menu.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-	"strconv"
+	"time"
 )
 
 func clearScreen() {
@@ -30,18 +30,16 @@ func input(message string) string {
 
 func menu() {
 	clearScreen()
+
 	fmt.Println("Movie Recommender")
 	fmt.Println("\n[1] Add a new movie")
-	fmt.Println("\n[2] Remove a movie")
-	fmt.Println("\n[3] Make the HTML")
+	fmt.Println("\n[2] Make the HTML")
 
 	value := input("\nSelect one of the options")
 	switch value {
 	case "1":
 		menuAddNewMovie()
 	case "2":
-		fmt.Println("2")
-	case "3":
 		makeHTML()
 	default:
 		menu()
@@ -51,34 +49,73 @@ func menu() {
 func menuAddNewMovie() {
 	clearScreen()
 
-	name := input("What is the name of the movie?")
+	var name string
+	for {
+		name = input("What is the name of the movie?")
+		if name != "" {
+			break
+		}
+	}
+
 	movies := getSearchMovies(name)
 	for i, movie := range movies {
 		fmt.Printf("[%d] %s (%d)\n", i+1, movie.Title, movie.Year)
-
 		if i >= 19 {
 			break
 		}
 	}
 
-	value := stringToInt(input("\nChoose movie from list above or enter [0] to perform a new search."))
-	if value <= 0 || value > 20 || value > int64(len(movies)) {
-		menuAddNewMovie()
-	} else {
-		date := input("When did you watch this movie? Enter the date as follows YYYYMMDD")
-		if _, err := strconv.Atoi(date); err != nil || len(date) != 8 {
-			date = "00000000"
-		}
-
-		var like int64
-		for {
-			like = stringToInt(input("\nDid you like the movie? [0] for No and [1] for Yes"))
-			if like == 0 || like == 1 {
-				break
-			}
-		}
-
-		movie := getMovie(movies[value-1].IMDB)
-		downloadCover(movie, date, like)
+	countOptions := int64(len(movies))
+	if countOptions > 20 {
+		countOptions = 20
 	}
+
+	var value int64
+	for {
+		valueString := input("\nChoose movie from list above or enter [0] to perform a new search.")
+
+		if valueString == "0" {
+			menuAddNewMovie()
+		}
+
+		value = stringToInt(valueString)
+		if value >= 1 && value <= countOptions {
+			break
+		}
+	}
+
+	var date string
+	for {
+		date = input("When did you watch this movie? Enter the date as follows YYYYMMDD. [] for today, [0] for an unknown date or type the date in the format already informed.")
+
+		if date == "" {
+			dateTime := time.Now()
+			date = dateTime.Format("20060102")
+			break
+		}
+
+		if date == "0" {
+			date = "00000000"
+			break
+		}
+
+		_, err := time.Parse("20060102", date)
+		if err == nil {
+			break
+		}
+	}
+
+	var like int64
+	for {
+		likeString := input("\nDid you like the movie? [0] for No and [1] for Yes")
+		if likeString == "0" || likeString == "1" {
+			like = stringToInt(likeString)
+			break
+		}
+	}
+
+	movie := getMovie(movies[value-1].IMDB)
+	downloadCover(movie, date, like)
+
+	os.Exit(0)
 }
